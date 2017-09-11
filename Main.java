0944054850e5e2ae2d501076ed9df8503eaa3cd7
Arnold <arnoldbuining@hotmail.com>
2017-09-11 18:13:52 +0200

    First commit and the solution to problem #1

---
+++ Main.java
@@ -0,0 +1,16 @@
+import Problems.Problem1;
+import Problems.Problem2;
+
+/**
+ * Created by Arnold on 11-9-2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+//        Problem1 problem1 = new Problem1();
+        Problem2 problem2 = new Problem2();
+
+    }
+
+}
---
+++ Problems/Problem1.java
@@ -0,0 +1,43 @@
+package Problems;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Arnold on 11-9-2017.
+ */
+public class Problem1 {
+
+    //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+    //The sum of these multiples is 23
+    //
+    //Find the sum of all the multiples of 3 or 5 below 1000.
+
+    protected int max = 1000;
+    protected int sum = 0;
+    protected ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+    public Problem1(){
+
+        //Place numbers
+        numbers.add(3);
+        numbers.add(5);
+
+        //Initial loop
+        for(int x=1;x<this.max;x++){
+            //Loop through the numbers
+            for(int i=0;i<numbers.size();i++){
+                //Is itterator in the table of the number?
+                if((x%numbers.get(i))==0){
+                    //Add it into the sum
+                    sum+=x;
+                    break;
+                }
+            }
+        }
+
+        //Print the solution
+        System.out.println(sum);
+
+    }
+
+}
