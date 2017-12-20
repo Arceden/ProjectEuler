43c7c20383767f93946b3f2bfcdd4b3164b386e3
Arnold <arnoldbuining@hotmail.com>
2017-12-20 12:48:02 +0100

    Added the problem description

--- Problems/Problem4.java
+++ Problems/Problem4.java
@@ -5,6 +5,10 @@ package Problems;
  */
 public class Problem4 extends Functions {
 
+    //A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit
+    //numbers is 9009 = 91 × 99.
+    //Find the largest palindrome made from the product of two 3-digit numbers.
+
     public Problem4(){
 
         int max = 1000;
---
+++ Problems/Problem5.java
@@ -0,0 +1,7 @@
+package Problems;
+
+/**
+ * Created by Arnold on 20-12-2017.
+ */
+public class Problem5 {
+}
