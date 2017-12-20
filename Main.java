7d08209f753962beabafc48438c9997207cc4920
Arnold <arnoldbuining@hotmail.com>
2017-12-20 14:52:23 +0100

    Todo: Problem 9

--- Main.java
+++ Main.java
@@ -7,14 +7,15 @@ public class Main {
 
     public static void main(String[] args){
 
-        Problem1 problem1 = new Problem1();
-        Problem2 problem2 = new Problem2();
-        Problem3 problem3 = new Problem3();
-        Problem4 problem4 = new Problem4();
-        Problem5 problem5 = new Problem5();   //This one may take a while to load
-        Problem6 problem6 = new Problem6();
-        Problem7 problem7 = new Problem7();
-        Problem8 problem8 = new Problem8();
+//        Problem1 problem1 = new Problem1();
+//        Problem2 problem2 = new Problem2();
+//        Problem3 problem3 = new Problem3();
+//        Problem4 problem4 = new Problem4();
+//        Problem5 problem5 = new Problem5();   //This one may take a while to load
+//        Problem6 problem6 = new Problem6();
+//        Problem7 problem7 = new Problem7();
+//        Problem8 problem8 = new Problem8();
+        Problem9 problem9 = new Problem9(); //TODO: this solution in general
 
     }
 
