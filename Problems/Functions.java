ecba2a7ee0c05a3b8a6e157d16041237c5d624e3
Arnold <arnoldbuining@hotmail.com>
2017-12-20 13:51:39 +0100

    Added a isPrime() function

--- Problems/Functions.java
+++ Problems/Functions.java
@@ -19,4 +19,13 @@ public class Functions {
 
     public void print(float msg) { System.out.println(prefix+msg); }
 
+    public boolean isPrime(int num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
+        return true;
+    }
+
 }
