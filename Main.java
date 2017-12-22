304622159441ec266a0d5ca7bf8cfeddef584de3
Arnold <arnoldbuining@hotmail.com>
2017-12-22 10:15:56 +0100

    Added a (so far) failed attempt of problem 14

--- Main.java
+++ Main.java
@@ -7,19 +7,20 @@ public class Main {
 
     public static void main(String[] args){
 
-        Problem1 problem1 = new Problem1();
-        Problem2 problem2 = new Problem2();
-        Problem3 problem3 = new Problem3();
-        Problem4 problem4 = new Problem4();
-//        Problem5 problem5 = new Problem5();   //This one may take a while to load
-        Problem6 problem6 = new Problem6();
-        Problem7 problem7 = new Problem7();
-        Problem8 problem8 = new Problem8();
-//        Problem9 problem9 = new Problem9(); //TODO: this solution in general
-        Problem10 problem10 = new Problem10();
-        Problem11 problem11 = new Problem11();
-        Problem12 problem12 = new Problem12();
-        Problem13 problem13 = new Problem13();
+//        Problem1 problem1 = new Problem1();
+//        Problem2 problem2 = new Problem2();
+//        Problem3 problem3 = new Problem3();
+//        Problem4 problem4 = new Problem4();
+//        Problem5 problem5 = new Problem5();       //TODO: This one may take a while to load, so recreate if possible
+//        Problem6 problem6 = new Problem6();
+//        Problem7 problem7 = new Problem7();
+//        Problem8 problem8 = new Problem8();
+//        Problem9 problem9 = new Problem9();       //TODO: this solution in general
+//        Problem10 problem10 = new Problem10();
+//        Problem11 problem11 = new Problem11();
+//        Problem12 problem12 = new Problem12();    //TODO: this solution in general
+//        Problem13 problem13 = new Problem13();
+        Problem14 problem14 = new Problem14();
 
     }
 
---
+++ Problems/Problem14.java
@@ -0,0 +1,61 @@
+package Problems;
+
+/**
+ * Created by Arnold on 22-12-2017.
+ */
+public class Problem14 extends Functions {
+
+    //The following iterative sequence is defined for the set of positive integers:
+    //
+    //n → n/2 (n is even)
+    //n → 3n + 1 (n is odd)
+    //
+    //Using the rule above and starting with 13, we generate the following sequence:
+    //
+    //        13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+    //It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+    //
+    //Which starting number, under one million, produces the longest chain?
+    //
+    //NOTE: Once the chain starts the terms are allowed to go above one million.
+
+    public Problem14(){
+
+        int highscore=0;
+        int highestStartingNum=0;
+        int max=1000000;
+
+        for(int x=13;x<max;x++){
+            int terms = getCollatzTerms(x);
+            if(terms>highscore){
+                highscore=terms;
+                highestStartingNum=x;
+            }
+        }
+
+        print(highestStartingNum);
+
+    }
+
+    public int getCollatzTerms(int num){
+
+        int terms = 1;      //Count the amount of terms.
+
+        while(num>1){       //Initial loop until the 1 has been hit
+
+            if(num%2==0){   //Even
+                num/=2;
+            } else {        //Odd
+                num*=3;
+                num+=1;
+            }
+
+            terms++;        //Another term has been done, so plus 1
+
+        }
+
+        return terms;       //Return the amount of terms
+
+    }
+
+}
