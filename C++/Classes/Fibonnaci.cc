c17d30424c18778f1a816cf0de5092e1d726007a
Arnold217 <arnoldbuining@hotmail.com>
2019-08-21 17:21:30 +0200

    Small preperations for Problem025

--- C++/Classes/Fibonnaci.cc
+++ C++/Classes/Fibonnaci.cc
@@ -2,41 +2,52 @@
 #include "Fibonnaci.h"
 
 Fibonnaci::Fibonnaci() {
-    tmp = 0;prev = 1;term = 0;
+    term = 0;
+    number = 1;
+    prev = 1;
+}
+
+/**
+ * Get the number corresponding number from the term
+ * (WIP)
+ */
+int Fibonnaci::getTerm(int term) {
+    if (term == 1 || term == 2) 
+        return 1;
+    else 
+        return 0;       
 }
 
 /**
  * Get next term from the Fibonnaci sequence
  */
 int Fibonnaci::next() {
-    if (term == 0) {
-        term = 1;
-    } else if (term == 1) {
-        term = 2;
-    } else {
-        tmp = term;
-        term = prev + term;
-        prev = tmp;
-    }
+    term ++;
 
-    return term;
+    if (term == 1 || term == 2)
+        return (number = 1);
+
+    int tmp = number;
+    number = prev + number;
+    prev = tmp;
+
+    return number;
 }
 
 /**
  * Loop _n_ amount of times through the sequence
  */
 void Fibonnaci::loop(int amount, std::function<void(int)> callback) {
-    for (int i=0; i<amount; i++) {
-        next();
-        callback(term);
+    for (uint64_t i=0; i<amount; i++) {
+        callback(next());
     }
 }
 
 /**
  * Keep looping until the term has reached its limit
  */
-void Fibonnaci::loopUntilLimit(int limit, std::function<void(int)> callback) {
+void Fibonnaci::loopUntilLimit(uint64_t limit, std::function<void(int)> callback) {
     while(next() < limit) {
-        callback(term);
+        callback(number);
     }
 }
--- C++/Classes/Fibonnaci.h
+++ C++/Classes/Fibonnaci.h
@@ -2,13 +2,14 @@
 
 class Fibonnaci {
     private:
+        int number;
         int prev;
-        int tmp;
 
     public:
         int term;
         Fibonnaci();
         int next();
+        int getTerm(int term);
         void loop(int amount, std::function<void(int)> callback);
-        void loopUntilLimit(int limit, std::function<void(int)> callback);
+        void loopUntilLimit(uint64_t limit, std::function<void(int)> callback);
 };
