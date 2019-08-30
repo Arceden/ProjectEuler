37afbb0d207173438d51af62de42211e3100b9b8
Arnold217 <arnoldbuining@hotmail.com>
2019-08-30 21:54:37 +0200

    Removed redundant code

--- Java/Problem028.java
+++ Java/Problem028.java
@@ -13,36 +13,28 @@ public class Problem028 implements Problem {
     /**
      * @param size > 2
      */
-    void getSpiral(int size) {
+    private void getSpiral(int size) {
         size /= 2;
         size++;
         int last = 0;
         int sum = 0;
 
-        LinkedList<Integer> numbers = new LinkedList<>();
-        LinkedList<Integer> diagonals = new LinkedList<>();
-
         // Get amount of spirals to make
         for (int i=0; i<size; i++) {
             int row = getAmountForSpiral(i);
 
-
             // Get numbers to push into the spiral row
             for (int x=0; x<row; x++) {
-                numbers.add(++last);
-
                 if (row == 1 || (x-((row/2)-1)) % (row/4) == 0) {
-                    diagonals.add(last);
                     sum += last;
                 }
             }
         }
 
-        System.out.println("Diagonals: \t" + diagonals);
         System.out.println("Sum: \t\t" + sum);
     }
 
-    int getAmountForSpiral(int n) {
+    private int getAmountForSpiral(int n) {
         if (n == 0)
             return 1;
 
